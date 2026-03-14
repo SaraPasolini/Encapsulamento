8d20d29088bf1180146c578b36846db3f876caf0
SaraPasolini <saraalmeida3606@gmail.com>
2026-03-13 23:05:00 -0300

    Exercício 02 completo e testado

--- Exercício02/Program.cs
+++ Exercício02/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Funcionario
+{
+    private string nome;
+    private double salario;
+
+    public string getNome()
+    {
+        return nome;
+    }
+
+    public double getSalario()
+    {
+        return salario;
+    }
+
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario)
+    {
+        this.salario = salario;
+    }
+
+    public void AumentarSalario(double percentual)
+    {
+        double aumento = salario * (percentual / 100);
+        salario += aumento;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setNome("Carlos");
+        funcionario.setSalario(2000);
+
+        double salarioInicial = funcionario.getSalario();
+        double percentual = 10;
+
+        funcionario.AumentarSalario(percentual);
+
+        Console.WriteLine("Funcionário: " + funcionario.getNome());
+        Console.WriteLine("Salário inicial: " + salarioInicial);
+        Console.WriteLine("Percentual de aumento: " + percentual + "%");
+        Console.WriteLine("Novo salário: " + funcionario.getSalario());
+    }
+}
